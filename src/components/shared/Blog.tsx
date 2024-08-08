6847dfe6cb26ccd5a17273ef282bd13c76d424d8
Iriboneye-Nina <ninairiboneye@gmail.com>
2024-08-08 12:44:39 +0200

    stylingUi

--- src/components/shared/Blog.tsx
+++ src/components/shared/Blog.tsx
@@ -14,27 +14,24 @@ const BlogPost: React.FC<BlogPostProps> = ({
   link,
 }) => {
   return (
-    <div className="max-w-64 rounded-lg overflow-hidden shadow-lg  bg-slate-100">
-      <div className="px-6 py-4">
-        <h2 role="heading" className="font-bold text-2xl mb-2 text-gray-800">
-          {title}
-        </h2>
-        <p role="description" className="text-gray-600 text-base mb-4">
-          {description}
-        </p>
-        <p role="date" className="text-gray-400 text-sm mb-4">
-          {date}
-        </p>
-        <a
-          href={link}
-          role="link"
-          className="text-indigo-500 hover:text-indigo-600 font-semibold"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Read More
-        </a>
-      </div>
+    <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md">
+      <h1 role="heading" className="text-2xl font-bold mb-2">
+        {title}
+      </h1>
+      <p role="description" className="text-gray-700 mb-4">
+        {description}
+      </p>
+      <p role="date" className="text-gray-500 mb-4">
+        {date}
+      </p>
+      <a
+        href={link}
+        role="link"
+        target="_blank"
+        className="text-blue-500 hover:underline"
+      >
+        Read more
+      </a>
     </div>
   );
 };
--- src/components/shared/BlogContent.tsx
+++ src/components/shared/BlogContent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BlogPost from "./Blog";
+import BlogPost from "@/components/shared/Blog";
 
 const Blog = () => {
   const posts = [
@@ -7,27 +7,26 @@ const Blog = () => {
       title: "My First Blog Post",
       description: "This is the description of my first blog post.",
       date: "2024-08-07",
-      link: "https://dev.to/iriboneye_nina_47b95feedc",
+      link: "https://dev.to/iriboneye_nina_47b95feedc/first-week-in-awesomit-4nb5",
     },
   ];
 
   return (
-    <div className="container mx-auto  px-4 py-8">
-      <h1 className="text-4xl font-bold text-center mb-12 text-white">
-        Blog post
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {posts.map((post, index) => (
-          <BlogPost
-            key={index}
-            title={post.title}
-            description={post.description}
-            date={post.date}
-            link={post.link}
-          />
-        ))}
+    <div className="bg-slate-100 min-h-screen flex items-center justify-center px-4 py-8">
+      <div className="container mx-auto">
+        
+          {posts.map((post, index) => (
+            <BlogPost
+              key={index}
+              title={post.title}
+              description={post.description}
+              date={post.date}
+              link={post.link}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+   
   );
 };
 
